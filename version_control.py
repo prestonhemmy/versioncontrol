57c29a2d16068445b7b922004269c18adbdd3d5d
prestonhemmy <149008060+prestonhemmy@users.noreply.github.com>
2023-10-25 21:24:11 -0400

    Add files via upload

---
+++ version_control.py
@@ -0,0 +1,39 @@
+# COP3502C: Lab 6: Version Control
+# Preston Hemmy
+
+def encode_password(password):
+    encoded_data = ''
+    for i in password:
+        encoded_data += str(int(i) + 3)
+    return encoded_data
+
+
+# def decode_password(encoded_password):
+#     decoded_data = ''
+#     for i in encoded_password:
+#         decoded_data += str(int(i) - 3)
+#     return decoded_datagit
+def decode_password(encoded_password):
+    pass
+
+
+if __name__ == '__main__':
+    user_encoded = ''
+    while True:
+        print('Menu\n'
+              '-------------\n'
+              '1. Encode\n'
+              '2. Decode\n'
+              '3. Quit\n')
+        option_select = input('Please enter an option: ')
+        if option_select != '3':
+            if option_select == '1':
+                user_password = input('Please enter your password to encode: ')
+                user_encoded = encode_password(user_password)
+                print('Your password has been encoded and stored!')
+            elif option_select == '2':
+                # user_decoded = decode_password(user_encoded)
+                # print(f'The encoded password is {user_encoded}, and the original password is {user_decoded}.')
+                pass
+        else:
+            exit()
